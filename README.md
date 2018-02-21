8f51b2bfc2c83350ec5ebdf2c358bfb7aa87f666
Valentin Brandl <vbrandl@riseup.net>
2018-02-21 14:56:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# Nginx HTTP2 Push Generator
+
+Since version 1.13.9 from 2018-02-20, the [nginx](https://www.nginx.com/) web server supports HTTP2
+server push. This can be a pain to configure since for every file the list of files to push has to
+be configured.
+
+This tool aims to automate the process of generating said list. It parses a HTML file, iterates over
+all nodes and looks for a set of predefined attributes (see below). If the value of the attribute
+seems to be a local path (e.g. does not contain `://` and does not start with `//`), the tool checks
+if the value is a relative or absolute path. Relative paths get prefixed by a user defined prefix,
+absolute paths are taken as is.
+The tool takes a list of file extensions as parameter and only resources ending in any of the
+extensions will be used in the configuration.
+
+## Checked Attributes
+
+Currently the following attributes are checked:
+
+ * `href`
+ * `src`
+
+## Usage
+
+```
+$ ./nginx_http2_push_generator -h
+nginx_http2_push_generator 0.1.0
+Valentin B. <mail@vbrandl.net>
+Parse a HTML file and generate a HTTP2 push configuration for nginx
+
+USAGE:
+    nginx_http2_push_generator [OPTIONS] <EXT>...
+
+FLAGS:
+    -h, --help       Prints help information
+    -V, --version    Prints version information
+
+OPTIONS:
+    -i, --input <FILE>       Input value. If not set or if set to "-", data is read from stdin
+    -p, --prefix <PREFIX>    Set a path prefix. Should be the path where the file is located. Defaults to "/"
+
+ARGS:
+    <EXT>...    File extensions to include
+```
+
+### Examples
+
+```
+$ ./nginx_http2_push_generator .css .js -i index.html -p /prefix
+http2_push /prefix/style1.css;
+http2_push /style2.css;
+http2_push /style3.css;
+http2_push /prefix/foo.js;
+```
+
+```
+$ cat index.html | ./nginx_http2_push_generator .css .js -p /prefix
+http2_push /prefix/style1.css;
+http2_push /style2.css;
+http2_push /style3.css;
+http2_push /prefix/foo.js;
+```
---
+++ src/main.rs
@@ -0,0 +1,80 @@
+#[macro_use]
+extern crate clap;
+#[macro_use]
+extern crate html5ever;
+
+use std::fs::File;
+use std::io::{self, BufReader, Write};
+
+use html5ever::parse_document;
+use html5ever::rcdom::{Handle, NodeData, RcDom};
+use html5ever::tendril::TendrilSink;
+
+const ATTRS: &[&str] = &["href", "src"];
+
+fn create_config(handle: Handle, prefix: &str, exts: &[&str]) {
+    let node = handle;
+    if let NodeData::Element {
+        ref name,
+        ref attrs,
+        ..
+    } = node.data
+    {
+        assert!(name.ns == ns!(html));
+        for attr in attrs.borrow().iter() {
+            let name = format!("{}", attr.name.local);
+            if ATTRS.iter().any(|a| name == *a) && !attr.value.starts_with("//")
+                && !attr.value.contains("://")
+                && exts.iter().any(|e| attr.value.ends_with(e))
+            {
+                print!("http2_push ");
+                if !attr.value.starts_with('/') {
+                    print!("{}/", prefix);
+                }
+                if attr.value.starts_with("./") {
+                    print!("{}", &attr.value[2..]);
+                } else {
+                    print!("{}", attr.value);
+                }
+                println!(";");
+            }
+        }
+    }
+
+    for child in node.children.borrow().iter() {
+        create_config(child.clone(), prefix, exts);
+    }
+}
+
+fn main() {
+    let matches = clap_app!(nginx_http2_push_generator =>
+                            (version: "0.1.0")
+                            (author: "Valentin B. <mail@vbrandl.net>")
+                            (about: "Parse a HTML file and generate a HTTP2 push configuration for nginx")
+                            (@arg PREFIX: -p --prefix +takes_value "Set a path prefix. Should be the path where the file is located. Defaults to \"/\"")
+                            (@arg FILE: -i --input +takes_value "Input value. If not set or if set to \"-\", data is read from stdin")
+                            (@arg EXT: ... +required "File extensions to include")
+                           ).get_matches();
+    let input = matches.value_of("FILE").unwrap_or("-");
+    let pref = matches.value_of("PREFIX").unwrap_or("");
+    let exts: Vec<_> = matches.values_of("EXT").unwrap().collect();
+    let dom = parse_document(RcDom::default(), Default::default()).from_utf8();
+    let stdin = io::stdin();
+    let dom = if input == "-" {
+        dom.read_from(&mut stdin.lock())
+    } else {
+        dom.read_from(&mut BufReader::new(File::open(input).unwrap()))
+    }.unwrap();
+    let stderr = io::stderr();
+    let mut stderr = stderr.lock();
+    if !dom.errors.is_empty() {
+        writeln!(
+            stderr,
+            "The following errors occurred when parsing the HTML file:"
+        ).ok();
+        dom.errors
+            .iter()
+            .for_each(|e| writeln!(stderr, "{}", e).unwrap());
+    }
+    create_config(dom.document, pref, &exts);
+}
